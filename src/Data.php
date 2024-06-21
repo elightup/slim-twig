90bf2ce35f95172ae8f94ad97698a882f39f3b89
LyNguyen <cnttnt@gmail.com>
2024-06-21 16:44:32 +0700

    Optimize code

--- src/Data.php
+++ src/Data.php
@@ -3,23 +3,6 @@ namespace SlimTwig;
 
 class Data {
 	/**
-	 * Render content with variables
-	 *
-	 * @param  string        $content
-	 * @param  array         $context
-	 * @return string
-	 */
-	public static function render( $content, array $context = [] ): string {
-		foreach( $context as $key => $value ) {
-			$new_value = htmlspecialchars( $value ?? '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8' );
-			$content = preg_replace('/\{\{' . $key . '\}\}/', $new_value, $content);
-			$content = preg_replace('/\{\{ ' . $key . ' \}\}/', $new_value, $content);
-		}
-
-		return $content;
-	}
-
-	/**
 	 * Get an item from an array or object using "dot" notation.
 	 * Similar to data_get() function in Laravel.
 	 *
--- src/Renderer.php
+++ src/Renderer.php
@@ -1,21 +1,12 @@
 <?php
 namespace SlimTwig;
 
-use \SlimTwig\Data;
-use \SlimTwig\Normalizer;
-
 class Renderer {
 	/**
-	 * Render a text.
-	 *
-	 * A text can be rendered as an array or a simple string.
-	 * Because of that, we have to render each variable and combine later.
+	 * Render a text with variables like {{ name.first_name }} {{ post.title }}.
 	 * 
-	 * @param  string        $text
-	 * @param  array         $data
-	 * @return string
 	 */
-	public static function render( string $text, array $data = [], $default = '' ): string {
+	public static function render( string $text, $data ): string {
 		// Don't render if no variables.
 		if ( ! str_contains( $text, '{{' ) ) {
 			// Parse shortcodes, blocks, etc.
@@ -23,54 +14,16 @@ class Renderer {
 		}
 
 		preg_match_all( '#{{\s*?([^}\s]+?)\s*?}}#', $text, $matches, PREG_PATTERN_ORDER );
-
-		// One variable.
-		if ( count( $matches[1] ) === 1 ) {
-			$var       = $matches[1][0];
-			$var_value = self::render_variable( $var, $data, $default );
-			// Allows to parse post.categories as array. In case of string, allow both dynamic and static texts.
-			$text = is_array( $var_value ) ? $var_value : strtr( $text, [ $var => $var_value ] );
-
-			// Parse shortcodes, blocks, etc.
-			return Normalizer::normalize( preg_replace( "/\{\{|\}\}/", "", $text ) );
-			
-		}
-
-		// If many variables in the text, render each one and replace later.
 		$replacements = [];
-		$count        = 1;
-		foreach ( $matches[1] as $var ) {
-			$var_value            = self::render_variable( $var, $data, $default );
-			$replacements[ $var ] = $var_value;
-			if ( is_array( $var_value ) ) {
-				$count = max( $count, count( $var_value ) );
-			}
+		foreach ( $matches[1] as $key => $variable ) {
+			$value = ( string )self::render_variable( $variable, $data );
+			$text = str_replace( $matches[0][ $key ], $value, $text);
 		}
-
-		$return = [];
-		for ( $i = 0; $i < $count; $i++ ) {
-			$row_replacements = [];
-			foreach ( $replacements as $var => $var_value ) {
-				$replacement              = is_array( $var_value ) ? ( $var_value[ $i ] ?? '' ) : $var_value;
-				$row_replacements[ $var ] = $replacement;
-			}
-			$return[] = strtr( $text, $row_replacements );
-		}
-		$text = $count === 1 ? reset( $return ) : $return;
-
-		// Parse shortcodes, blocks, etc.
-		return Normalizer::normalize( preg_replace( "/\{\{|\}\}/", "", $text ) );
+		return Normalizer::normalize( $text );
 	}
 
-	/**
-	 * Render variable with variables
-	 *
-	 * @param  string        $content
-	 * @param  array         $context
-	 * @return string
-	 */
-	private static function render_variable( string $vari, array $data, string $default ): string {
-		return Data::get( $data, $vari ) ?? $default;
+	private static function render_variable( string $vari, $data ): string {
+		return Data::get( $data, $vari, '' );
 	}
 }
 
