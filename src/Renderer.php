2897d0fa03212e56f337601ec8b6b98834a9f776
LyNguyen <cnttnt@gmail.com>
2024-06-21 17:04:42 +0700

    Optimize code

--- src/Renderer.php
+++ src/Renderer.php
@@ -14,16 +14,16 @@ class Renderer {
 		}
 
 		preg_match_all( '#{{\s*?([^}\s]+?)\s*?}}#', $text, $matches, PREG_PATTERN_ORDER );
-		$replacements = [];
 		foreach ( $matches[1] as $key => $variable ) {
 			$value = ( string )self::render_variable( $variable, $data );
 			$text = str_replace( $matches[0][ $key ], $value, $text);
 		}
+
 		return Normalizer::normalize( $text );
 	}
 
-	private static function render_variable( string $vari, $data ): string {
-		return Data::get( $data, $vari, '' );
+	private static function render_variable( string $variable, $data ): string {
+		return Data::get( $data, $variable, '' );
 	}
 }
 
