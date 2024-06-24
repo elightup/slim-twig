36664ba8812b3abb1fc3065a8848334be6318632
Anh Tran <anhtnt@elightup.com>
2024-06-24 15:22:44 +0700

    Improve renderer

--- src/Renderer.php
+++ src/Renderer.php
@@ -3,10 +3,10 @@ namespace SlimTwig;
 
 class Renderer {
 	public static function render( string $text, $data ): string {
-		preg_match_all( '#{{\s*?([^}\s]+?)\s*?}}#', $text, $matches, PREG_PATTERN_ORDER );
-		foreach ( $matches[1] as $key => $variable ) {
-			$value = (string) self::render_variable( $variable, $data );
-			$text  = str_replace( $matches[0][ $key ], $value, $text );
+		preg_match_all( '#{{\s*?([^}\s]+?)\s*?}}#', $text, $matches, PREG_SET_ORDER );
+		foreach ( $matches as $variable ) {
+			$value = (string) self::render_variable( $variable[1], $data );
+			$text  = str_replace( $variable[0], $value, $text );
 		}
 		return $text;
 	}
