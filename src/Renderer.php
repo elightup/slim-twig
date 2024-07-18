04ebb63b2d6c857ce842a1f089803abd4a020d80
Anh Tran <anhtnt@elightup.com>
2024-07-18 11:56:26 +0700

    Avoid warning when type cast an array to string

--- src/Renderer.php
+++ src/Renderer.php
@@ -5,7 +5,11 @@ class Renderer {
 	public static function render( string $text, $data ): string {
 		preg_match_all( '#{{\s*?([^}\s]+?)\s*?}}#', $text, $matches, PREG_SET_ORDER );
 		foreach ( $matches as $variable ) {
-			$value = (string) self::render_variable( $variable[1], $data );
+			$value = self::render_variable( $variable[1], $data );
+			if ( is_array( $value ) ) {
+				$value = implode( ', ', $value );
+			}
+			$value = (string) $value;
 			$text  = str_replace( $variable[0], $value, $text );
 		}
 		return $text;
